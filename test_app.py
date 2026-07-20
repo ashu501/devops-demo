89187fb786a899514a97f5ba44dabfe41d9434ba
Aswini <ashwinipvc@gmail.com>
2026-07-20 15:27:30 -0800

    updated

--- test_app.py
+++ test_app.py
@@ -5,12 +5,12 @@ from app import add, subtract, multiply, greet
 
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 4
     assert add(-1, 1) == 0
 
 
 def test_subtract():
-    assert subtract(5, 2) == 3
+    assert subtract(5, 2) == 6
     assert subtract(0, 5) == -5
 
 
